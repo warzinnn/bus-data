19f558c57bbd9d3cac42bf5222b3d7b4da0c7f4e
warzinnn <vkimura1@gmail.com>
2023-06-27 17:25:51 -0300

    fixing conflict bugs with spark and airflow

--- docker/manage_docker.sh
+++ docker/manage_docker.sh
@@ -3,7 +3,9 @@ if [[ $1 == "--up" ]]; then
     docker-compose -f $(pwd)/1-kafka/docker-compose.yml up -d
     sleep 5
     docker-compose -f $(pwd)/2-spark/docker-compose.yml --env-file ../.env up -d 
+    docker-compose -f $(pwd)/../airflow/docker-compose.yaml up -d
 elif [[ $1 == "--down" ]]; then
+    docker-compose -f $(pwd)/../airflow/docker-compose.yaml down
     docker-compose -f $(pwd)/2-spark/docker-compose.yml down
     docker-compose -f$(pwd)/1-kafka/docker-compose.yml down
 else
